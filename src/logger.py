e0b9ff34527bc45e4260bc5fae8ded23520e75cf
Liam Major <liam.major91@gmail.com>
2021-02-21 01:21:02 -0500

    added the Logger class which will perform all the logging this takes an outputFile as its params

--- src/logger.py
+++ src/logger.py
@@ -4,5 +4,25 @@ This is the module that will contain all the code for the logging process.
 It will be a neat wrapper that can be interfaced with via. multithreading and the webcrawler
 '''
 
-def f2():
-    print("imported from logger.py")
+# comment this later teehee
+
+from .consts import Constants
+
+class Logger:
+    def __init__(self, verbose: bool = Constants.verbose, outputFile: str or bool = False):
+        self._fout = outputFile
+
+        # Verifying params
+        if not isinstance(self._active, bool):
+            raise ValueError("isLogging can only be of type boolean not of type %s" % type(self._active))
+        if not self._fout and self._fout != True:
+            if not isinstance(self._fout, str):
+                raise ValueError("outputFile can only be of type string or False not of type %s" % type(self._fout)) 
+        
+    def logToFile(self, string: str):
+        # in the future it'll just self._fout.write(string)
+
+        print(string)
+
+    def isLogging(self):
+        return isinstance(self._fout, str) and not self._fout
