e6bc573856873f11583c53c5a9b8c85b80dd741c
Liam Major <liam.major91@gmail.com>
2021-02-21 01:17:14 -0500

    fleshed out some of the database commands configured the constructor to currently have a database name

--- src/db.py
+++ src/db.py
@@ -3,5 +3,14 @@
 This is the module that contains the database for all the websites visited.
 '''
 
-def f1 ():
-    print("imported from db.py")
+# comment this later teehee
+
+from .consts import Constants
+
+class Database:
+    def __init__(self, dbName: str or bool = False):
+        self._ref = dbName
+
+    def isDatabase(self):
+        # returns a true or false there is a database reference passed
+        return (self._ref != False)
