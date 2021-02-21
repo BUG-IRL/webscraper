91059ca0c28d353d53fa44daa84706711ca4c359
Liam Major <liam.major91@gmail.com>
2021-02-21 14:51:22 -0500

    commented class methods changed naming convention from _ to camel case

--- src/crawler.py
+++ src/crawler.py
@@ -9,12 +9,14 @@ This module will handle opening links and retriving the anchor tags from it.
 
 # imports
 
-from .consts import pv
 import re
 import urllib.request as u
 
+# local imports
+
 from .logger import Logger 
 from .db import Database
+from .consts import pv
 
 # consts from here
 
@@ -35,28 +37,40 @@ class Crawler:
         print("Made crawler")
 
     def isLogging(self):
-        if self.hasLogger():
-            return self._logger.isLogging()
-        else:
-            return False
+        if self.hasLogger(): return self._logger.isLogging()
+        else: return False
 
     def hasLogger(self):
+        """
+        This function returns true or false whether the crawler has a logger or not
+
+        Returns:
+            true if has logger, false otherwise
+        """
         return isinstance(self._logger, Logger)
 
     def hasDatabase(self):
+        """
+        This function returns true or false whether the crawler has a database or not
+
+        Returns:
+            true if has database, false otherwise
+        """
         return isinstance(self._db, Database)
 
     def log(self, string: str):
         if self.hasLogger():
-            print("called Crawler.log with a logger")
             self._logger.logToFile(string)
+            return True
+        else:
+            return False
 
 # defining functions
 
-def scrape_attrs(a):
+def scrapeAttrs(a):
     """
     This function scrapes the attributes from an HTML element
-    
+
     Parameters:
         a (str): The HTML element to be scraped in string form
 
@@ -76,7 +90,7 @@ def scrape_attrs(a):
 
     return r
 
-def scrape_anchors(string):
+def scrapeAnchors(string: str, pattern: str = anchor_pattern):
     """
     A generator function which yields an anchor tag
     
@@ -87,21 +101,19 @@ def scrape_anchors(string):
         Yields a string object
     """
 
-    global anchor_pattern
-
-    for i in re.findall(anchor_pattern, string):
+    for i in re.findall(pattern, string):
         for j in i.split("<a"):
             if not j: 
                 continue
             yield ("<a" + j)
 
-def sanitize_anchors(l, response: u.http.client.HTTPResponse):
+def sanitizeAnchors(l, response: u.http.client.HTTPResponse):
     """
     This function scrapes the attributes from an HTML element
     
     '/new' --> 'https://youtube.com/new'
     Parameters:
-        l (List of dicts): Takes an array of output from scrape_attrs
+        l (List of dicts): Takes an array of output from scrapeAttrs
 
     Returns:
         A list of dicts [{}, {}, {}]
@@ -125,7 +137,7 @@ def sanitize_anchors(l, response: u.http.client.HTTPResponse):
     return r
 
 
-def get_all_anchors(s: u.http.client.HTTPResponse):
+def getAllAnchors(s: u.http.client.HTTPResponse):
     """
     This function takes the string of an HTTPResponse and scrapes all
     the anchor tags and returns them as a python dict
@@ -139,10 +151,10 @@ def get_all_anchors(s: u.http.client.HTTPResponse):
 
     r = []
 
-    for a in scrape_anchors(str(s.read())):
+    for a in scrapeAnchors(str(s.read())):
         try:
-            r.append(scrape_attrs(a))
+            r.append(scrapeAttrs(a))
         except Exception as e:
-            pass
+            pv("Caught exception during getAllAnchors: %s" % str(e))
 
-    return sanitize_anchors(r, s)
+    return sanitizeAnchors(r, s)
