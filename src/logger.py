5b18fc7050306f44489c00c24054c0a72838cb75
Liam Major <liam.major91@gmail.com>
2021-02-21 14:51:52 -0500

    removed deprecated kwarg pruned excess space

--- src/logger.py
+++ src/logger.py
@@ -13,15 +13,11 @@ class Logger:
         self._fout = outputFile
 
         # Verifying params
-        if not isinstance(self._active, bool):
-            raise ValueError("isLogging can only be of type boolean not of type %s" % type(self._active))
-        if not self._fout and self._fout != True:
-            if not isinstance(self._fout, str):
-                raise ValueError("outputFile can only be of type string or False not of type %s" % type(self._fout)) 
+        if not isinstance(self._fout, str) and self._fout != False:
+            raise ValueError("outputFile can only be of type string or False not of type %s" % type(self._fout)) 
         
     def logToFile(self, string: str):
         # in the future it'll just self._fout.write(string)
-
         print(string)
 
     def isLogging(self):
