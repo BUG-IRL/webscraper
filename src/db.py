8f15b7bd44406d9a7ed0a3e2959a0cf40612d6e5
Liam Major <liam.major91@gmail.com>
2021-02-21 15:13:29 -0500

    added param verification

--- src/db.py
+++ src/db.py
@@ -5,11 +5,15 @@ This is the module that contains the database for all the websites visited.
 
 # comment this later teehee
 
-from .consts import Constants
+from .consts import Constants, paramError
 
 class Database:
     def __init__(self, dbName: str or bool = False):
         self._ref = dbName
+
+        # param checking
+        if not isinstance(self._ref, str) or self._ref == True:
+            paramError('dbName', ['str', 'False'], type(self._ref))
 
     def isDatabase(self):
         # returns a true or false there is a database reference passed
