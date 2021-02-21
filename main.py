cba8c774a03a3440be873d417d744e99d27c65c7
Liam Major <liam.major91@gmail.com>
2021-02-21 14:50:58 -0500

    pruned test code and added code to test logging function

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from src import db, logger, crawler
 
 # with urllib.request.urlopen(href) as response:
 
-#     scraped = crawler.get_all_anchors(response)
+#     scraped = crawler.getAllAnchors(response)
 
 #     for index, i in enumerate(scraped):
 #         print(index, i)
@@ -26,25 +26,10 @@ from src import db, logger, crawler
 l = logger.Logger(outputFile="h")
 d = db.Database(dbName=True)
 
-hasLogger = crawler.Crawler(logger=l)
-print("%s isLogging func %s" %("hasLogger", str(hasLogger.isLogging()))) # True
-print("%s hasLogger func %s" %("hasLogger", str(hasLogger.hasLogger()))) # True
-print("%s hasDatabase func %s" %("hasLogger", str(hasLogger.hasDatabase()))) # False
-print()
-
-hasDatabase = crawler.Crawler(db=d)
-print("%s isLogging func %s" %("hasDatabase", str(hasDatabase.isLogging()))) # False
-print("%s hasLogger func %s" %("hasDatabase", str(hasDatabase.hasLogger()))) # False
-print("%s hasDatabase func %s" %("hasDatabase", str(hasDatabase.hasDatabase()))) # True
-print()
-
 hasBoth = crawler.Crawler(logger=l, db=d)
 print("%s isLogging func %s" %("hasBoth", str(hasBoth.isLogging()))) # True
 print("%s hasLogger func %s" %("hasBoth", str(hasBoth.hasLogger()))) # True
 print("%s hasDatabase func %s" %("hasBoth", str(hasBoth.hasDatabase()))) # True
 print()
 
-hasNeither = crawler.Crawler()
-print("%s isLogging func %s" %("hasNeither", str(hasNeither.isLogging()))) # False
-print("%s hasLogger func %s" %("hasNeither", str(hasNeither.hasLogger()))) # False
-print("%s hasDatabase func %s" %("hasNeither", str(hasNeither.hasDatabase()))) # False
+hasBoth.log("logged something lmao")
