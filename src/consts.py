d9327b341e1227753841ec7b93b483b46621d32c
Liam Major <liam.major91@gmail.com>
2021-02-21 15:13:09 -0500

    made a new function paramError takes 3 args: paramName, allowedTypes and typeReceived and raises a new TypeError based on those args

--- src/consts.py
+++ src/consts.py
@@ -16,3 +16,13 @@ class Constants:
 def pv(s: str): # pv ---> print verbose
     if Constants.verbose:
         print(s)
+
+def paramError(paramName: str, allowedTypes: list or str, typeReceived: str):
+    if isinstance(allowedTypes, list) or isinstance(allowedTypes, tuple):
+        typeString = ", ".join(str(T) for T in allowedTypes)
+    elif isinstance(allowedTypes, str):
+        typeString = allowedTypes
+    else:
+        raise TypeError("allowedTypes can be of type list or str not type %s" % type(allowedTypes))
+
+    raise TypeError(f"{paramName} can only be of type {typeString} not of type {typeReceived}")  
